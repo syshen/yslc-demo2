fdf5718aae0fb45623c1b10fa484d31c866d0ccc
Steven Shen <sysheen@gmail.com>
2024-07-16 11:55:30 +0800

    change order page text color

--- app/order/[order_id]/page.tsx
+++ app/order/[order_id]/page.tsx
@@ -49,13 +49,13 @@ export default async function OrderPage({ params }: { params: { order_id: string
 
   return (
     <MantineProvider>
-      <div className="p-6 border border-gray-200 rounded-3xl w-full group transition-all duration-500 hover:border-gray-400 ">
+      <div className="p-6 border border-gray-200 w-full group transition-all duration-500">
         <Box className="shadow-sm my-5">
           <h2
-            className="font-manrope font-bold text-3xl leading-10 text-black pb-6">
+            className="font-manrope font-bold text-3xl leading-10 pb-6">
             訂單內容
           </h2>
-          <Group className="flex flex-row justify-between border-b">
+          <Group className="flex flex-row justify-between border-b py-5">
             <p>{new Date(order.created_at).toLocaleDateString()}</p>
             <p className="font-italic text-md text-right">{getStatus(order.state)}</p>
           </Group>
@@ -64,24 +64,24 @@ export default async function OrderPage({ params }: { params: { order_id: string
           {order.items.map((item) => (
             <div className="flex items-center justify-between gap-4 mb-5">
               <div className="flex flex-col">
-                <p className="font-normal text-lg leading-8 text-black transition-all duration-500 group-hover:text-gray-700">{item.item}</p>
-                <p className="font-noraml text-lg leading-8 text-black transition-all duration-500 group-hover:text-gray-700">數量: {item.quantity}</p>
+                <p className="font-normal text-lg leading-8 transition-all duration-500">{item.item}</p>
+                <p className="font-noraml text-lg leading-8 transition-all duration-500">數量: {item.quantity}</p>
               </div>
-              <p className="font-medium text-lg leading-8 text-gray-900">{Number(item.subtotal).toLocaleString()}</p>
+              <p className="font-medium text-lg leading-8">{Number(item.subtotal).toLocaleString()}</p>
             </div>
           ))}
         </div>
         <div className="total flex items-center justify-between pt-6">
-          <p className="font-normal text-lg leading-8 text-black ">未稅價</p>
-          <h5 className="font-manrope font-bold text-lg leading-9 text-gray-400">{Number(untax_total).toLocaleString()}</h5>
+          <p className="font-normal text-lg leading-8">未稅價</p>
+          <h5 className="font-manrope font-bold text-lg leading-9">{Number(untax_total).toLocaleString()}</h5>
         </div>
         <div className="total flex items-center justify-between pt-6">
-          <p className="font-normal text-lg leading-8 text-black ">稅金</p>
-          <h5 className="font-manrope font-bold text-lg leading-9 text-gray-400">{Number(Math.round(untax_total * order.tax)).toLocaleString()}</h5>
+          <p className="font-normal text-lg leading-8">稅金</p>
+          <h5 className="font-manrope font-bold text-lg leading-9">{Number(Math.round(untax_total * order.tax)).toLocaleString()}</h5>
         </div>
         <div className="total flex items-center justify-between pt-6">
-          <p className="font-normal text-xl leading-8 text-black ">總價</p>
-          <h5 className="font-manrope font-bold text-2xl leading-9 text-indigo-600">{Number(order.total).toLocaleString()}</h5>
+          <p className="font-normal text-xl leading-8">總價</p>
+          <h5 className="font-manrope font-bold text-2xl leading-9">{Number(order.total).toLocaleString()}</h5>
         </div>
       </div>
     </MantineProvider>
