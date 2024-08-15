01e6e7944c2d6ffbcb0ec9d9a6d8213bcd7f653c
Steven Shen <sysheen@gmail.com>
2024-08-15 17:50:11 +0800

    prevent re-entry

--- app/shop/[mode]/[customer_id]/[order_id]/page.tsx
+++ app/shop/[mode]/[customer_id]/[order_id]/page.tsx
@@ -2,15 +2,30 @@
 
 import { useState, useEffect } from 'react';
 import { useDisclosure } from '@mantine/hooks';
-import { MantineProvider, Text, Box, Group, Button, Modal } from '@mantine/core';
+import {
+  MantineProvider,
+  Text,
+  Box,
+  Group,
+  Button,
+  Modal,
+} from '@mantine/core';
 import { createClient } from '@/utils/supabase/client';
-import { Product, Customer, PaymentOption } from '@/utils/types';
+import {
+  Product,
+  Customer,
+  PaymentOption,
+  Order,
+  OrderState,
+  PaymentState,
+} from '@/utils/types';
 import { shopCarts } from '@/app/actions';
 
 export default function OrderPage(
   { params }: { params: { mode: string, customer_id: string, order_id: string } }
 ) {
   const { mode, customer_id, order_id } = params;
+  const [order, setOrder] = useState<Order>();
   const supabase = createClient();
   const [rows, setRows] = useState<JSX.Element[]>([]);
   interface Cart {
@@ -109,17 +124,57 @@ export default function OrderPage(
       setRows(rs);
   };
 
+  const getOrder = async () => {
+    const { data } = await supabase
+    .from('orders')
+    .select('*')
+    .eq('customer_id', customer_id)
+    .eq('order_id', order_id);
+    if (data) {
+      const [o] = data;
+      setOrder(o);
+    }
+  };
+
+  const orderDisabled = (o:Order | undefined) => {
+    if (o === undefined) {
+      return false;
+    }
+    if (o.state === OrderState.CANCELLED
+    || o.state === OrderState.COMPLETED
+    || o.state === OrderState.SHIPPED
+    || o.state === OrderState.DELIVERED) {
+      return true;
+    }
+    if (o.payment_status === PaymentState.PAID) {
+      return true;
+    }
+    return false;
+  };
+
   useEffect(() => {
     listProducts();
   }, [products, cart, customer]);
 
   useEffect(() => {
+    getOrder();
     getCustomer();
     getProducts();
   }, []);
 
   return (
     <MantineProvider>
+      <Modal
+        opened={orderDisabled(order)}
+        onClose={close}
+        closeOnClickOutside={false}
+        closeOnEscape={false}
+        withCloseButton={false}
+        centered
+      >
+        <h2>無法重複下訂</h2>
+        <p>該筆訂單已在處理中，請重新送訊息下訂，謝謝</p>
+      </Modal>
       <Modal
         opened={opened}
         onClose={close}
