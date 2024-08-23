8c83aea83fd1b0df5f280a0cff4767cf7465c4bb
Steven Shen <sysheen@gmail.com>
2024-08-23 15:15:19 +0800

    test

--- app/shop/[mode]/[customer_id]/[order_id]/page.tsx
+++ app/shop/[mode]/[customer_id]/[order_id]/page.tsx
@@ -193,6 +193,7 @@ export default function OrderPage(
 
   return (
     <MantineProvider>
+      <Text>{JSON.stringify(carts)}</Text>
       <Modal
         opened={orderDisabled(order)}
         onClose={close}
